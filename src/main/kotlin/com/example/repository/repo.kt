8267ab9f7e55f29f6ba8201b57d03051e6325214
Ахунов Денис <denis.akhunov00@mail.ru>
2022-02-15 01:19:01 +0500

    Add basic login to notes

--- src/main/kotlin/com/example/repository/repo.kt
+++ src/main/kotlin/com/example/repository/repo.kt
@@ -1,14 +1,16 @@
 package com.example.repository
 
+import com.example.data.model.Note
 import com.example.data.model.User
+import com.example.data.table.NoteTable
 import com.example.data.table.UserTable
 import com.example.repository.DatabaseFactory.dbQuery
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 
 
 class Repo {
+
+    // TODO: User
 
     suspend fun addUser(user:User){
         dbQuery{
@@ -35,6 +37,73 @@ class Repo {
             email =  row[UserTable.email],
             hashPassword = row[UserTable.hashPassword],
             userName = row[UserTable.name]
+        )
+    }
+
+    // TODO: Notes
+
+    suspend fun addNotes(note: Note,email: String){
+
+        dbQuery {
+
+            NoteTable.insert {
+
+                it[id] = note.id
+                it[userEmail] = email
+                it[noteTitle] = note.noteTitle
+                it[description] = note.description
+                it[date] = note.date
+
+            }
+
+        }
+    }
+
+    suspend fun getAllNotes(email: String): List<Note> = dbQuery {
+
+        NoteTable.select{
+            NoteTable.userEmail.eq(email)
+        }.mapNotNull { rowToNote(it) }
+    }
+
+    suspend fun updateNote(note: Note, email: String){
+
+        dbQuery {
+
+            NoteTable.update (
+                where = {
+                    NoteTable.userEmail.eq(email) and NoteTable.id.eq(note.id)
+                }
+            ){
+
+                it[noteTitle] = note.noteTitle
+                it[description] = note.description
+                it[date] = note.date
+
+            }
+        }
+    }
+
+    suspend fun deleteNote(id: String){
+
+        dbQuery {
+
+            NoteTable.deleteWhere { NoteTable.id.eq(id) }
+        }
+
+    }
+
+    private fun rowToNote(row: ResultRow?): Note? {
+
+        if (row == null ){
+            return null
+        }
+
+        return Note(
+            id = row[NoteTable.id],
+            noteTitle = row[NoteTable.noteTitle],
+            description = row[NoteTable.description],
+            date = row[NoteTable.date]
         )
     }
 
